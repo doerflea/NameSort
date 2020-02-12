e98a010493e4c86251e7d79c3a3b5b8e60aded76
Anneliese Doerfler <anneliesedoerfler@gmail.com>
2020-02-12 09:45:39 -0800

    clean up

--- sort.cpp
+++ sort.cpp
@@ -4,15 +4,14 @@
 #include <sstream>
 #include <algorithm>
 
-using namespace std;
-
 struct person{
-   string first_name, last_name;
+   std::string first_name, last_name;
 };
 
-/* Overload "<" operator to compare names alphatebically by last name.
- * If last names match, then first names are compared*/
-bool operator < (const struct person &p1, const struct person &p2){
+/* Compare names alphatebically by last name.
+ * If last names match, then first names are compared
+ * If first individual is alphabetically before the 2nd individual true is returned*/
+bool comp (const struct person &p1, const struct person &p2){
 
    if(p1.last_name < p2.last_name){
       return true;
@@ -26,38 +25,33 @@ bool operator < (const struct person &p1, const struct person &p2){
 }
 
 
-/*Compare two people alphabetically*/
-bool comp(const struct person &p1, const struct person &p2){
-   return p1 < p2;
-}
-
 
 int main(){
 
-   ifstream file_input;
-   ofstream file_output;
+   std::ifstream file_input;
+   std::ofstream file_output;
 
-   string file_name;
+   std::string file_name;
 
    //Allow user to enter file name
-   cout << "Enter file name to open (a sample file called names.txt is provided within directory). File: ";
-   cin >> file_name;
+   std::cout << "Enter file name to open (a sample file called names.txt is provided within directory). File: ";
+   std::cin >> file_name;
 
    file_input.open(file_name.c_str());
 
    //Invalid files will be reprompted
    while(file_input.fail()){
-      cout << endl << "An error occured while opening the file" << std::endl;
-      cout << "Enter file name to open: ";
-      cin >> file_name;
+      std::cout << std::endl << "An error occured while opening the file" << std::endl;
+      std::cout << "Enter file name to open: ";
+      std::cin >> file_name;
       file_input.open(file_name.c_str());
    }
 
    if(file_input.good()){
 
-      vector<struct person>people;
+      std::vector<struct person>people;
 
-      string str;
+      std::string str;
 
       /*Get each indiviual's first and last names. Assumptions:
       	  -names are given in a file formatted as first name then last name
@@ -65,8 +59,8 @@ int main(){
 	  -each individual is on a seperate line*/
 
       while(getline(file_input,str)){ 
-	 istringstream ss(str);
-	 string fn,ln;
+	 std::istringstream ss(str);
+	 std::string fn,ln;
 	 ss >> fn; //get first name
 	 ss >> ln; //get last name
 	 struct person p = {fn,ln}; //create person object
@@ -75,18 +69,18 @@ int main(){
 
       file_input.close();
 
-      sort(people.begin(), people.end(),comp); //sort names alphabetically by last name
+      std::sort(people.begin(), people.end(),comp); //sort names alphabetically by last name
 
       //Output sorted names to new sorted_names.txt file
       file_output.open("sorted_names.txt");
 
       for(int i = 0; i < people.size(); i++){
-         file_output << people[i].last_name << " " << people[i].first_name << endl;
+         file_output << people[i].last_name << " " << people[i].first_name << std::endl;
 
       }
       file_output.close();
 
-      cout << endl <<  "File sucessfully sorted. Sorted names file is located at sorted_names.txt" << endl;
+      std::cout << std::endl <<  "File sucessfully sorted. Sorted names file is located at sorted_names.txt" << std::endl;
    }
    return 0;
 }
